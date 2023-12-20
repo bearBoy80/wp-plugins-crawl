ba50162261e6751f4b34fd604f10b821ce53b2c7
bearboy80 <2373319771@qq.com>
2023-12-20 12:00:38 +0800

    first commit

---
+++ src/test/java/com/github/wp/pluginscrawl/PluginsCrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wp.pluginscrawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginsCrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
